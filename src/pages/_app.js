9493b3410e1f5e08719c711fb1b8260bdb2d47c7
hithesh svsk <hiteshsunkara@eworldes.com>
2023-11-05 22:28:17 +0530

    redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import rootReducer from "@/store";
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const Store = createStore(rootReducer);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={Store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
