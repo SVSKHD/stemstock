0b9b696354da27b96b16bfb9c6989a9c0130a7d8
hithesh svsk <hiteshsunkara@eworldes.com>
2023-11-19 19:40:24 +0530

    user forget password

--- src/pages/api/user/forgetPassword.js
+++ src/pages/api/user/forgetPassword.js
@@ -1,6 +1,6 @@
 import { createRouter } from "next-connect";
 import mongoose from "mongoose";
-import User from "../../models/UserModel"; // Adjust the import path as necessary
+import User from "@/backend/models/user"; // Adjust the import path as necessary
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import db from "@/backend/db/db";
@@ -12,8 +12,8 @@ App.post(async (req, res) => {
 
   try {
     // Connect to MongoDB
-    
-    db.connectDb()
+
+    db.connectDb();
 
     // Check if user exists
     const user = await User.findOne({ email: email.toLowerCase() });
@@ -33,23 +33,20 @@ App.post(async (req, res) => {
 
     // Respond with a message
     res.status(200).send("Password reset link has been sent to your email.");
-    db.disconnectDb()
+    db.disconnectDb();
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server error");
-    db.disconnectDb()
+    db.disconnectDb();
   }
 });
 
 export default App.handler();
 
-
 // NEXT_PUBLIC_API_DB = "mongodb+srv://hithesh:hithesh@aqua.6swj0.mongodb.net/"
 // NEXT_PUBLIC_API_URL= "http://localhost:4000/api"
 // NEXT_PUBLIC_JWT_SECRET="stemstock"
 
-
 // # kite connect
 // NEXT_PUBLIC_API_ZERODHA_KEY = 2g58vdwzg7xhqkk7
 // NEXT_PUBLIC_API_ZERODHA_SECRET = 5zyx5mdfltk3illcdv6fj7pq6zujy4e1
-
