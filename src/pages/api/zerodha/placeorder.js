b4445dc611777e3d10cd13cf6047ff6e7e72242c
hithesh svsk <hiteshsunkara@eworldes.com>
2023-11-28 22:53:00 +0530

    place order

--- src/pages/api/zerodha/placeorder.js
+++ src/pages/api/zerodha/placeorder.js
@@ -1,16 +1,66 @@
+// // File: /pages/api/placeOrders.js
+// import { createRouter } from "next-connect";
+// import KiteConnect from "kiteconnect";
+
+// const router = createRouter();
+
+// // Configure your Kite Connect API key and secret
+// const apiKey = process.env.NEXT_PUBLIC_API_ZERODHA_KEY;
+// const apiSecret = process.env.NEXT_PUBLIC_API_ZERODHA_SECRET;
+
+// router.post(async (req, res) => {
+//     res.json(req.body)
+//     const { requestToken, legs } = req.body; // Assuming 'legs' is part of the request body
+
+//     const kite = new KiteConnect({
+//         api_key: apiKey
+//     });
+
+//     try {
+//         const session = await kite.generateSession(requestToken, apiSecret);
+//         kite.setAccessToken(session.access_token);
+
+//         // Function to place an order for a single leg
+//         const placeOrderForLeg = async (leg) => {
+//             const orderDetails = {
+//                 exchange: "NSE",
+//                 tradingsymbol: leg.instrument,
+//                 transaction_type: "BUY",
+//                 quantity: leg.quantity,
+//                 order_type: "MARKET",
+//                 product: leg.product,
+//                 price: leg.price,
+//               };
+
+//             return await kite.placeOrder("regular", orderDetails);
+//         };
+
+//         // Placing orders for each leg
+//         const orderResponses = await Promise.all(legs.map(leg => placeOrderForLeg(leg)));
+
+//         // Sending back the response
+//         res.status(200).json(orderResponses);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+// export default router.handler();
+
+
 // File: /pages/api/placeOrders.js
 import { createRouter } from "next-connect";
-import KiteConnect from "kiteconnect";
+import  KiteConnect  from "kiteconnect";
+
 
 const router = createRouter();
 
-// Configure your Kite Connect API key and secret
-const apiKey = "YOUR_API_KEY";
-const apiSecret = "YOUR_API_SECRET";
+// Use environment variables for API key and secret
+const apiKey = process.env.NEXT_PUBLIC_API_ZERODHA_KEY;
+const apiSecret = process.env.NEXT_PUBLIC_API_ZERODHA_SECRET;
 
 router.post(async (req, res) => {
-    res.json(req.body)
-    const { requestToken, legs } = req.body; // Assuming 'legs' is part of the request body
+    const { requestToken, legs } = req.body;
 
     const kite = new KiteConnect({
         api_key: apiKey
@@ -20,25 +70,27 @@ router.post(async (req, res) => {
         const session = await kite.generateSession(requestToken, apiSecret);
         kite.setAccessToken(session.access_token);
 
-        // Function to place an order for a single leg
-        const placeOrderForLeg = async (leg) => {
-            const orderDetails = {
-                exchange: "NSE",
-                tradingsymbol: leg.instrument,
-                transaction_type: "BUY",
-                quantity: leg.quantity,
-                order_type: "MARKET",
-                product: leg.product,
-                price: leg.price,
-              };
+        const orderResponses = [];
 
-            return await kite.placeOrder("regular", orderDetails);
-        };
+        for (const leg of legs) {
+            try {
+                const orderParams = {
+                    exchange: leg.exchange || "NSE", // Default to NSE if not specified
+                    tradingsymbol: leg.instrument,
+                    transaction_type: leg.position === "SELL" ? "SELL" : "BUY", // Adjust based on leg position
+                    quantity: parseInt(leg.quantity),
+                    order_type: leg.order_type || "MARKET", // Default to MARKET if not specified
+                    product: leg.product || "CNC", // Default to CNC if not specified
+                    price: parseFloat(leg.price) || 0, // Default to 0 if not specified
+                };
 
-        // Placing orders for each leg
-        const orderResponses = await Promise.all(legs.map(leg => placeOrderForLeg(leg)));
+                const response = await kite.placeOrder("regular", orderParams);
+                orderResponses.push({ legId: leg._id, orderId: response.order_id, status: "success" });
+            } catch (orderError) {
+                orderResponses.push({ legId: leg._id, status: "failed", error: orderError.message });
+            }
+        }
 
-        // Sending back the response
         res.status(200).json(orderResponses);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -46,3 +98,5 @@ router.post(async (req, res) => {
 });
 
 export default router.handler();
+
+
